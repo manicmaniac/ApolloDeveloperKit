c299968a30c4c6201b07dc653ef7cc7221231b1e
Ryosuke Ito <rito.0305@gmail.com>
2019-06-20 03:17:53 +0900

    fix memory leak of url request

--- Sources/Classes/EventStreamQueue.swift
+++ Sources/Classes/EventStreamQueue.swift
@@ -6,28 +6,34 @@
 //  Copyright © 2019 Ryosuke Ito. All rights reserved.
 //
 
-struct EventStreamQueue<Key: Hashable> {
-    private var queue = [Key: [EventStreamChunk]]()
+struct EventStreamQueue<Key: AnyObject> {
+    private var queuesByKey = NSMapTable<Key, NSMutableArray>.weakToStrongObjects()
     private var lock = NSLock()
 
     mutating func enqueue(chunk: EventStreamChunk, forKey key: Key) {
         lock.lock()
         defer { lock.unlock() }
-        queue[key, default: []].append(chunk)
+        let queue = queuesByKey.object(forKey: key)
+        if let queue = queue {
+            queue.add(chunk)
+        } else {
+            queuesByKey.setObject(NSMutableArray(object: chunk), forKey: key)
+        }
     }
 
     mutating func enqueueForAllKeys(chunk: EventStreamChunk) {
-        for key in queue.keys {
-            enqueue(chunk: chunk, forKey: key)
+        for key in queuesByKey.keyEnumerator() {
+            enqueue(chunk: chunk, forKey: key as! Key)
         }
     }
 
     mutating func dequeue(key: Key) -> EventStreamChunk? {
         lock.lock()
         defer { lock.unlock() }
-        if queue[key]?.isEmpty == true {
-            return nil
+        if let queue = queuesByKey.object(forKey: key), let chunk = queue.firstObject as? EventStreamChunk {
+            queue.removeObject(at: 0)
+            return chunk
         }
-        return queue[key]?.remove(at: 0)
+        return nil
     }
 }
