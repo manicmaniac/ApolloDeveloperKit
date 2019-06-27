72890723b5d3446965c7e4f6f2ded3a656c2a403
Ryosuke Ito <rito.0305@gmail.com>
2019-06-28 08:54:14 +0900

    rewrite with XCTContext

--- Tests/Classes/DebuggableNetworkTransportTests.swift
+++ Tests/Classes/DebuggableNetworkTransportTests.swift
@@ -11,66 +11,60 @@ import XCTest
 @testable import ApolloDeveloperKit
 
 class DebuggableNetworkTransportTests: XCTestCase {
-    private var networkTransport: DebuggableNetworkTransport!
-
-    func testSendOperationWithCompletionHandler_whenResponseIsNilAndErrorIsNil() {
+    func testSendOperationWithCompletionHandler() {
         let operation = MockGraphQLQuery()
-        let response: GraphQLResponse<MockGraphQLQuery>? = nil
-        let networkTransport = DebuggableNetworkTransport(networkTransport: MockNetworkTransport(response: response, error: nil))
-        let expectation = self.expectation(description: "completionHandler should be called")
-        let cancellable = networkTransport.send(operation: operation) { response, error in
-            XCTAssertNil(response)
-            XCTAssertNil(error)
-            expectation.fulfill()
+        XCTContext.runActivity(named: "when response is nil and error is nil") { _ in
+            let response: GraphQLResponse<MockGraphQLQuery>? = nil
+            let networkTransport = DebuggableNetworkTransport(networkTransport: MockNetworkTransport(response: response, error: nil))
+            let expectation = self.expectation(description: "completionHandler should be called")
+            let cancellable = networkTransport.send(operation: operation) { response, error in
+                XCTAssertNil(response)
+                XCTAssertNil(error)
+                expectation.fulfill()
+            }
+            XCTAssertTrue(cancellable is MockCancellable)
+            waitForExpectations(timeout: 0.25, handler: nil)
         }
-        XCTAssertTrue(cancellable is MockCancellable)
-        waitForExpectations(timeout: 0.25, handler: nil)
-    }
-
-    func testSendOperationWithCompletionHandler_whenResponseIsNotNilAndErrorIsNil() {
-        let operation = MockGraphQLQuery()
-        let response = GraphQLResponse<MockGraphQLQuery>(operation: operation, body: ["foo": "bar"])
-        let networkTransport = DebuggableNetworkTransport(networkTransport: MockNetworkTransport(response: response, error: nil))
-        let expectation = self.expectation(description: "completionHandler should be called")
-        let cancellable = networkTransport.send(operation: operation) { response, error in
-            XCTAssertNotNil(response)
-            XCTAssertEqual(response?.body.count, 1)
-            XCTAssertNil(error)
-            expectation.fulfill()
+        XCTContext.runActivity(named: "when response is not nil but error is nil") { _ in
+            let response = GraphQLResponse<MockGraphQLQuery>(operation: operation, body: ["foo": "bar"])
+            let networkTransport = DebuggableNetworkTransport(networkTransport: MockNetworkTransport(response: response, error: nil))
+            let expectation = self.expectation(description: "completionHandler should be called")
+            let cancellable = networkTransport.send(operation: operation) { response, error in
+                XCTAssertNotNil(response)
+                XCTAssertEqual(response?.body.count, 1)
+                XCTAssertNil(error)
+                expectation.fulfill()
+            }
+            XCTAssertTrue(cancellable is MockCancellable)
+            waitForExpectations(timeout: 0.25, handler: nil)
         }
-        XCTAssertTrue(cancellable is MockCancellable)
-        waitForExpectations(timeout: 0.25, handler: nil)
-    }
-
-    func testSendOperationWithCompletionHandler_whenResponseIsNilAndErrorIsNotNil() {
-        let operation = MockGraphQLQuery()
-        let response: GraphQLResponse<MockGraphQLQuery>? = nil
-        let urlError = URLError(.badURL)
-        let networkTransport = DebuggableNetworkTransport(networkTransport: MockNetworkTransport(response: response, error: urlError))
-        let expectation = self.expectation(description: "completionHandler should be called")
-        let cancellable = networkTransport.send(operation: operation) { response, error in
-            XCTAssertNil(response)
-            XCTAssertTrue(error as NSError? === urlError as NSError)
-            expectation.fulfill()
+        XCTContext.runActivity(named: "when response is nil and error is not nil") { _ in
+            let response: GraphQLResponse<MockGraphQLQuery>? = nil
+            let urlError = URLError(.badURL)
+            let networkTransport = DebuggableNetworkTransport(networkTransport: MockNetworkTransport(response: response, error: urlError))
+            let expectation = self.expectation(description: "completionHandler should be called")
+            let cancellable = networkTransport.send(operation: operation) { response, error in
+                XCTAssertNil(response)
+                XCTAssertTrue(error as NSError? === urlError as NSError)
+                expectation.fulfill()
+            }
+            XCTAssertTrue(cancellable is MockCancellable)
+            waitForExpectations(timeout: 0.25, handler: nil)
         }
-        XCTAssertTrue(cancellable is MockCancellable)
-        waitForExpectations(timeout: 0.25, handler: nil)
-    }
-
-    func testSendOperationWithCompletionHandler_whenResponseIsNotNilAndErrorIsNotNil() {
-        let operation = MockGraphQLQuery()
-        let response = GraphQLResponse<MockGraphQLQuery>(operation: operation, body: ["foo": "bar"])
-        let urlError = URLError(.badURL)
-        let networkTransport = DebuggableNetworkTransport(networkTransport: MockNetworkTransport(response: response, error: urlError))
-        let expectation = self.expectation(description: "completionHandler should be called")
-        let cancellable = networkTransport.send(operation: operation) { response, error in
-            XCTAssertNotNil(response)
-            XCTAssertEqual(response?.body.count, 1)
-            XCTAssertTrue(error as NSError? === urlError as NSError)
-            expectation.fulfill()
+        XCTContext.runActivity(named: "when response is not nil and error is not nil") { _ in
+            let response = GraphQLResponse<MockGraphQLQuery>(operation: operation, body: ["foo": "bar"])
+            let urlError = URLError(.badURL)
+            let networkTransport = DebuggableNetworkTransport(networkTransport: MockNetworkTransport(response: response, error: urlError))
+            let expectation = self.expectation(description: "completionHandler should be called")
+            let cancellable = networkTransport.send(operation: operation) { response, error in
+                XCTAssertNotNil(response)
+                XCTAssertEqual(response?.body.count, 1)
+                XCTAssertTrue(error as NSError? === urlError as NSError)
+                expectation.fulfill()
+            }
+            XCTAssertTrue(cancellable is MockCancellable)
+            waitForExpectations(timeout: 0.25, handler: nil)
         }
-        XCTAssertTrue(cancellable is MockCancellable)
-        waitForExpectations(timeout: 0.25, handler: nil)
     }
 }
 
