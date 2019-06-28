5dec89703b0c6a7ade0bb7b968f42268c79c3455
Ryosuke Ito <rito.0305@gmail.com>
2019-06-29 02:26:39 +0900

    add tests for EventStreamQueueMap

--- Sources/Classes/EventStream/EventStreamQueueMap.swift
+++ Sources/Classes/EventStream/EventStreamQueueMap.swift
@@ -6,8 +6,16 @@
 //  Copyright © 2019 Ryosuke Ito. All rights reserved.
 //
 
-class EventStreamQueueMap<Key: AnyObject> {
+public class EventStreamQueueMap<Key: AnyObject> {
     private let queuesByKey = NSMapTable<Key, EventStreamQueue>.weakToStrongObjects()
+
+    var count: Int {
+        return queuesByKey.keyEnumerator().allObjects.count
+    }
+
+    var isEmpty: Bool {
+        return count == 0
+    }
 
     func enqueue(chunk: EventStreamChunk, forKey key: Key) {
         if let queue = queuesByKey.object(forKey: key) {
