395e7600282200b8cc9378b340cfbe6b3f0930b1
Ryosuke Ito <rito.0305@gmail.com>
2019-09-29 01:04:36 +0900

    Refactor apollo_version.rb

--- lib/apollo_version.rb
+++ lib/apollo_version.rb
@@ -4,6 +4,12 @@ require 'shellwords'
 require 'yaml'
 
 class ApolloVersion
+  class NotFoundError < StandardError
+    def initialize(msg = 'Apollo.framework not found.')
+      super(msg)
+    end
+  end
+
   include Comparable
 
   attr_reader :version
@@ -11,28 +17,29 @@ class ApolloVersion
   alias_method :to_s, :version
 
   def initialize(version)
-    raise "'#{version}' cannot be parsed as a version." unless Gem::Version.correct?(version)
+    raise(TypeError, 'version cannot be nil.') if version.nil?
+    raise(ArgumentError, "'#{version}' cannot be parsed as a version.") unless version.empty? || Gem::Version.correct?(version)
     @version = version
   end
 
   def self.find!
-    find_in_podfile_lock || find_in_frameworks || raise('Apollo.framework not found.')
-  end
-
-  def self.find_in_frameworks(framework_search_paths = nil)
-    framework_search_paths ||= ENV['FRAMEWORK_SEARCH_PATHS'].shellsplit
-    Find.find(*framework_search_paths) do |path|
-      if path.end_with?('Apollo.framework/Info.plist')
-        version = parse_version_from_info_plist(path)
-        return new(version) if version
-      end
+    begin
+      find_in_podfile_lock!
+    rescue
+      find_in_frameworks!
     end
   end
 
-  def self.find_in_podfile_lock(podfile_lock_path = nil)
-    podfile_lock_path ||= File.expand_path('../Podfile.lock', ENV['PODS_ROOT'])
-    version = parse_version_from_podfile_lock(podfile_lock_path)
-    new(version) if version
+  def self.find_in_frameworks!(framework_search_paths = default_framework_search_paths)
+    Find.find(*framework_search_paths) do |path|
+      next unless path.end_with?('Apollo.framework/Info.plist')
+      return new(parse_version_from_info_plist(path))
+    end
+    raise(NotFoundError.new)
+  end
+
+  def self.find_in_podfile_lock!(podfile_lock_path = default_podfile_lock_path)
+    new(parse_version_from_podfile_lock(podfile_lock_path))
   end
 
   def <=>(other)
@@ -42,18 +49,27 @@ class ApolloVersion
 
   private
 
+  def self.default_framework_search_paths
+    ENV.fetch('FRAMEWORK_SEARCH_PATHS', '').shellsplit
+  end
+
+  def self.default_podfile_lock_path
+    pods_root = ENV['PODS_ROOT']
+    File.expand_path('../Podfile.lock', pods_root) if pods_root
+  end
+
   def self.parse_version_from_info_plist(path)
-    `/usr/libexec/PlistBuddy -c 'Print :CFBundleVersion' #{path.shellescape}`.chomp
+    output = `/usr/libexec/PlistBuddy -c 'Print :CFBundleVersion' #{path.shellescape}`.chomp
+    output.empty? ? raise(NotFoundError.new) : output
   end
 
   def self.parse_version_from_podfile_lock(path)
-    yaml = YAML.load(File.read(path))
-    yaml['PODS'].lazy
-                .map { |entry| entry.is_a?(Hash) ? entry.keys.first : entry }
-                .map { |pod| pod.match(/^Apollo \(([0-9.]+)\)$/) }
-                .reject(&:nil?)
-                .first[1]
-  rescue
-    nil
+    pods = YAML.load(File.read(path))['PODS']
+    pods.each do |pod|
+      pod = pod.keys.first if pod.is_a?(Hash)
+      match_data = pod.match(/^Apollo \(([0-9.]+)\)$/)
+      return match_data[1] if match_data
+    end
+    raise(NotFoundError.new)
   end
 end
