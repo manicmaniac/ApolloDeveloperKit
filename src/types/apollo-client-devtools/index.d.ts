ea9e2550a864882fd001d6b5c7931338882e49b1
Ryosuke Ito <rito.0305@gmail.com>
2019-11-05 06:53:43 +0900

    Add header comment

--- src/types/apollo-client-devtools/index.d.ts
+++ src/types/apollo-client-devtools/index.d.ts
@@ -1,3 +1,8 @@
+// Type definitions for apollo-client-devtools 2.2.5
+// Project: https://github.com/apollographql/apollo-client-devtools
+// Definitions by: Ryosuke Ito <rito.0305@gmail.com>
+// TypeScript Version: 3.5.2
+
 import { Hook } from 'apollo-client-devtools/src/backend/hook';
 
 declare global {
