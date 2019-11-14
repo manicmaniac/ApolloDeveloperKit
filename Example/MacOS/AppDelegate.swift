ea9594edfcb2d880e2b095061fa14ee0368be505
Ryosuke Ito <rito.0305@gmail.com>
2019-11-14 10:17:54 +0900

    Add empty macOS example target

---
+++ Example/MacOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ApolloDeveloperKitExample-MacOS
+//
+//  Created by Ryosuke Ito on 11/14/19.
+//  Copyright © 2019 Ryosuke Ito. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Example/MacOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ApolloDeveloperKitExample-MacOS
+//
+//  Created by Ryosuke Ito on 11/14/19.
+//  Copyright © 2019 Ryosuke Ito. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
