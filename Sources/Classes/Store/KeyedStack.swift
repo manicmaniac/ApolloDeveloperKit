9114f0c1b39b03b3bcea1699b8944b44444a228c
Ryosuke Ito <rito.0305@gmail.com>
2020-08-25 04:22:52 +0900

    Add documentation comment about unusual behavior

--- Sources/Classes/Store/KeyedStack.swift
+++ Sources/Classes/Store/KeyedStack.swift
@@ -6,6 +6,11 @@
 //  Copyright © 2020 Ryosuke Ito. All rights reserved.
 //
 
+/**
+ * `KeyedStack` is kind of a stack of `Value`s keyed by `Key`.
+ *
+ * - Warning: Iterating over `KeyedStack` yields key-value pairs from the *bottom* of stack.
+ */
 struct KeyedStack<Key, Value> where Key: Equatable {
     private var elements = [(Key, Value)]()
 
@@ -13,6 +18,12 @@ struct KeyedStack<Key, Value> where Key: Equatable {
         elements.append((key, value))
     }
 
+    /**
+     * Access the value associated with the given key for reading and writing.
+     *
+     * Since `KeyedStack` is a stack and doesn't guarantee uniqueness of keys,
+     * the return value is the *first* found `Value` from the top of  stack.
+     */
     subscript(key: Key) -> Value? {
         get {
             guard let index = elements.lastIndex(where: { k, _ in k == key }) else { return nil }
