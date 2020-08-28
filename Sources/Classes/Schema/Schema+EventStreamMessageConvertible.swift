d7d78f853b7f8007909dcf2dddbc1b41c724f9ab
Ryosuke Ito <rito.0305@gmail.com>
2020-08-29 07:55:57 +0900

    Move schema to dedicated directory

--- Sources/Classes/Schema/Schema+EventStreamMessageConvertible.swift
+++ Sources/Classes/Schema/Schema+EventStreamMessageConvertible.swift
@@ -1,10 +1,16 @@
 //
-//  StateChange+EventStreamMessageConvertible.swift
+//  Schema+EventStreamMessageConvertible.swift
 //  ApolloDeveloperKit
 //
 //  Created by Ryosuke Ito on 8/29/20.
 //  Copyright © 2020 Ryosuke Ito. All rights reserved.
 //
+
+extension ConsoleEvent: EventStreamMessageConvertible {
+    var message: EventStreamMessage {
+        return EventStreamMessage(event: String(describing: type), data: data)
+    }
+}
 
 extension StateChange: EventStreamMessageConvertible {
     var message: EventStreamMessage {
