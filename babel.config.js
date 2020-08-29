5cefb590de5899a08763cbbd415d834f6cd93d66
Ryosuke Ito <rito.0305@gmail.com>
2020-08-29 19:42:58 +0900

    Remove direct dependency to babel
    Since apollo-client-devtools is distributed in ES5 and it internally
    uses babel, ApolloDeveloperKit need to depend on babel as compile time
    dependency although apollo-client-devtools is only used for integration
    testing.
    
    This commit force apollo-client-devtools compiled with tsc.
    Though it is theoretically wrong for the module compiled with babel to be
    compiled with tsc, actually it is safe because apollo-client-devtools uses
    babel only to translate the latest JavaScript syntax for older browser.
    
    So I decided to quit depending on babel to reduce dependencies.

--- babel.config.js
+++
@@ -1,5 +0,0 @@
-// Since apollo-client-devtools uses babel internally, this config file is necessary to run integration tests.
-// ApolloDeveloperKit uses tsc command itself to compile .ts files so it doesn't need babel except for testing.
-module.exports = {
-  presets: ["@babel/preset-env"]
-};
