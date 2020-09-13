97258b43267e0207b3ae7cb104ecab076440005a
Ryosuke Ito <rito.0305@gmail.com>
2020-09-13 15:53:42 +0900

    Remove trailing semicolons

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
   mode: 'production',
@@ -18,4 +18,4 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'Sources/Assets')
   }
-};
+}
