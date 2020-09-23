f2ff74034918320e15123016e9611ad6bcffff54
Ryosuke Ito <rito.0305@gmail.com>
2020-09-24 03:33:59 +0900

    Add tests for notification in ConsoleRedirection

--- Tests/Classes/Console/ConsoleRedirectionTests.swift
+++ Tests/Classes/Console/ConsoleRedirectionTests.swift
@@ -13,35 +13,125 @@ class ConsoleRedirectionTests: XCTestCase {
     private var notificationCenter: NotificationCenter!
     private var delegateHandler: ConsoleRedirectionDelegateHandler!
     private var mockDuplicator: MockFileDescriptorDuplicator!
+    private var temporaryDirectoryURL: URL!
+    private var standardOutputFileHandle: FileHandle!
+    private var standardErrorFileHandle: FileHandle!
 
     override func setUp() {
         notificationCenter = NotificationCenter()
         delegateHandler = ConsoleRedirectionDelegateHandler()
         mockDuplicator = MockFileDescriptorDuplicator()
+        let fileManager = FileManager.default
+        do {
+            temporaryDirectoryURL = try fileManager.url(for: .itemReplacementDirectory,
+                                                        in: .userDomainMask,
+                                                        appropriateFor: URL(fileURLWithPath: NSTemporaryDirectory()),
+                                                        create: true)
+            let standardOutputURL = temporaryDirectoryURL.appendingPathComponent("stdout")
+            fileManager.createFile(atPath: standardOutputURL.path, contents: nil)
+            standardOutputFileHandle = try FileHandle(forWritingTo: standardOutputURL)
+            let standardErrorURL = temporaryDirectoryURL.appendingPathComponent("stderr")
+            fileManager.createFile(atPath: standardErrorURL.path, contents: nil)
+            standardErrorFileHandle = try FileHandle(forWritingTo: standardErrorURL)
+        } catch let error {
+            continueAfterFailure = false
+            XCTFail(String(describing: error))
+        }
+    }
+
+    override func tearDown() {
+        do {
+            try FileManager.default.removeItem(at: temporaryDirectoryURL)
+        } catch let error {
+            continueAfterFailure = false
+            XCTFail(String(describing: error))
+        }
     }
 
     func testInit() {
-        let consoleRedirection = ConsoleRedirection(notificationCenter: notificationCenter, queue: .main, duplicator: mockDuplicator)
+        let consoleRedirection = ConsoleRedirection(standardOutputFileDescriptor: stdout,
+                                                    standardErrorFileDescriptor: stderr,
+                                                    notificationCenter: notificationCenter,
+                                                    queue: .main,
+                                                    duplicator: mockDuplicator)
         consoleRedirection.addObserver(delegateHandler!, selector: #selector(delegateHandler.didReceiveConsoleDidWriteNotification(_:)))
-        XCTAssertEqual(mockDuplicator.dupInvocationHistory, [1, 2])
+        XCTAssertEqual(mockDuplicator.dupInvocationHistory, [stdout, stderr])
         guard mockDuplicator.dup2InvocationHistory.count == 2 else {
             return XCTFail("dup2 must be called exactly twice but called \(mockDuplicator.dup2InvocationHistory.count) times.")
         }
-        XCTAssertEqual(mockDuplicator.dup2InvocationHistory[0].fildes2, 1)
-        XCTAssertEqual(mockDuplicator.dup2InvocationHistory[1].fildes2, 2)
+        XCTAssertEqual(mockDuplicator.dup2InvocationHistory[0].fildes2, stdout)
+        XCTAssertEqual(mockDuplicator.dup2InvocationHistory[1].fildes2, stderr)
         consoleRedirection.removeObserver(delegateHandler!)
     }
 
     func testDeinit() {
-        let consoleRedirection = ConsoleRedirection(notificationCenter: notificationCenter, queue: .main, duplicator: mockDuplicator)
+        let consoleRedirection = ConsoleRedirection(standardOutputFileDescriptor: stdout,
+                                                    standardErrorFileDescriptor: stderr,
+                                                    notificationCenter: notificationCenter,
+                                                    queue: .main,
+                                                    duplicator: mockDuplicator)
         consoleRedirection.addObserver(delegateHandler!, selector: #selector(delegateHandler.didReceiveConsoleDidWriteNotification(_:)))
         mockDuplicator.clearInvocationHistory()
         consoleRedirection.removeObserver(delegateHandler!)
         guard mockDuplicator.dup2InvocationHistory.count == 2 else {
             return XCTFail("dup2 must be called exactly twice but called \(mockDuplicator.dup2InvocationHistory.count) times.")
         }
-        XCTAssertEqual(mockDuplicator.dup2InvocationHistory[0].fildes2, 1)
-        XCTAssertEqual(mockDuplicator.dup2InvocationHistory[1].fildes2, 2)
+        XCTAssertEqual(mockDuplicator.dup2InvocationHistory[0].fildes2, stdout)
+        XCTAssertEqual(mockDuplicator.dup2InvocationHistory[1].fildes2, stderr)
+    }
+
+    func testNotification_whenWritingToStandardOutput() throws {
+        let consoleRedirection = ConsoleRedirection(standardOutputFileDescriptor: stdout,
+                                                    standardErrorFileDescriptor: stderr,
+                                                    notificationCenter: notificationCenter,
+                                                    queue: .main,
+                                                    duplicator: DarwinFileDescriptorDuplicator())
+        consoleRedirection.addObserver(delegateHandler!, selector: #selector(delegateHandler.didReceiveConsoleDidWriteNotification(_:)))
+        let data = Data("foo".utf8)
+        let expectation = self.expectation(description: "Callback should be called.")
+        delegateHandler.consoleDidWriteDataToDestinationCallback = { receivedConsoleRedirection, receivedData, receivedDestination in
+            defer { expectation.fulfill() }
+            XCTAssert(receivedConsoleRedirection === consoleRedirection)
+            XCTAssertEqual(receivedData, data)
+            XCTAssertEqual(receivedDestination, .standardOutput)
+        }
+        if #available(macOS 10.15, *, iOS 13.4, *) {
+            try standardOutputFileHandle.write(contentsOf: data)
+        } else {
+            standardOutputFileHandle.write(data)
+        }
+        waitForExpectations(timeout: 0.25)
+    }
+
+    func testNotification_whenWritingToStandardError() throws {
+        let consoleRedirection = ConsoleRedirection(standardOutputFileDescriptor: stdout,
+                                                    standardErrorFileDescriptor: stderr,
+                                                    notificationCenter: notificationCenter,
+                                                    queue: .main,
+                                                    duplicator: DarwinFileDescriptorDuplicator())
+        consoleRedirection.addObserver(delegateHandler!, selector: #selector(delegateHandler.didReceiveConsoleDidWriteNotification(_:)))
+        let data = Data("foo".utf8)
+        let expectation = self.expectation(description: "Callback should be called.")
+        delegateHandler.consoleDidWriteDataToDestinationCallback = { receivedConsoleRedirection, receivedData, receivedDestination in
+            defer { expectation.fulfill() }
+            XCTAssert(receivedConsoleRedirection === consoleRedirection)
+            XCTAssertEqual(receivedData, data)
+            XCTAssertEqual(receivedDestination, .standardError)
+        }
+        if #available(macOS 10.15, *, iOS 13.4, *) {
+            try standardErrorFileHandle.write(contentsOf: data)
+        } else {
+            standardErrorFileHandle.write(data)
+        }
+        waitForExpectations(timeout: 0.25)
+    }
+
+    private var stdout: Int32 {
+        return standardOutputFileHandle.fileDescriptor
+    }
+
+    private var stderr: Int32 {
+        return standardErrorFileHandle.fileDescriptor
     }
 }
 
