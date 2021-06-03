9f95737d3ee8452bd6f4d21a7cbf0677a7cf693b
Ryosuke Ito <rito.0305@gmail.com>
2021-06-04 03:07:14 +0900

    Modify package description

--- Package.swift
+++ Package.swift
@@ -1,28 +1,26 @@
 // swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "ApolloDeveloperKit",
+    platforms: [.iOS(.v9), .macOS(.v10_10)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ApolloDeveloperKit",
             targets: ["ApolloDeveloperKit"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "Apollo", url: "https://github.com/apollographql/apollo-ios.git", "0.34.0"..<"0.35.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ApolloDeveloperKit",
-            dependencies: []),
+            dependencies: ["Apollo"],
+            resources: [.copy("Assets")]),
         .testTarget(
             name: "ApolloDeveloperKitTests",
-            dependencies: ["ApolloDeveloperKit"]),
+            dependencies: ["ApolloDeveloperKit"],
+            exclude: ["ApolloDeveloperKitTests.swift"]),
     ]
 )
